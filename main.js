420c85c72f310233eb1717536f722dcd167bb716
Marina Sov <sovic.marina@gmail.com>
2014-11-13 16:39:45 -0800

    Organizing code, changing style

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-function Question(question, options, correct){
-	this.question = question;
-	this.options = options;
-	this.correct = correct;
-}
-
-var items = [new Question ("What is the second biggest country in the world?", ["Russia", "Canada", "China", "USA"], 1),
-			new Question ("What is the Capital of Kazahstan?", ["Tashkent", "Sucre", "Astana", "Ashgabat"], 2),
-			new Question ("Who is the fastest land animal?", ["Horse", "Cheetah", "Lion", "Kangaro"], 1),
-			new Question ("What is the speed of sound?", ["340 m/s", "3.6 km/h", "1020 m/s", "32 m/s"], 0),
-			new Question ("Which planet has the most moons?", ["Neptune", "Saturn", "Uranus", "Jupiter"], 3),
-			new Question ("What is debut studio album by the American rock band Nirvana?", ["Come as you are", "Nevermind", "In Utero", "Bleach"], 3),
-			new Question ("What film didn't win 11 Oscar awards?", ["The Return of the King ", "Ben-Hur", "Gone With The Wind", "Titanic"], 2),
-			new Question ("How many players are on a baseball team?", ["7", "11", "9", "10"], 2),
-			new Question ("What is the heaviest living reptile?", ["Aldabra giant tortoise", "Orinoco crocodile", "Nile crocodile", "Saltwater crocodile"], 3),
-			new Question ("During what Chinese dinasty was paper invented?", ["Han", "Quin", "Xin", "Ming"], 0),
-			new Question ("In which century did Western Roman Empire end?", ["5 AD", "4 BC", "7 AD", "6 AD"], 0),
-			new Question ("In which century did Francesco Petrarca live?", ["11", "12", "13", "14"], 3),
-			new Question ("Who was FIFA World Cup winner 1994?", ["Germany", "France", "Brasil", "Argentina"], 2),
-			new Question ("What is the capital of Bangladesh?", ["Hanoi", "Islamabad", "Dhaka", "Kathmandu"], 2),
-			new Question ("Where is La Guajira Desert?", ["In Columbia", "In Peru", "In Argentina", "In Chile"], 0)
-			];
-			
 $(document).ready(function(){
 	
 	var state = {
@@ -30,75 +7,85 @@ $(document).ready(function(){
 		clicked : 0
 	};
 	
-	$(".start.command").click(function() {
-	  onStart(state);
+	var dialogEnd = $(".dialogEnd");
+	
+	$(".dialogStart button").click(function() {
+		onStart(state);
 	});	
 	
 	$(".choose").click(function() {
-	  onChoose(state, this.id);
+		onChoose(state, this.id);
 	});
 	
-	$(".next.command").click(function() { 
+	$(".dialogQuestions button").click(function() { 
 		onNext(state);
 	});
 	
-	$(".again.command").click(function() {
+	$(".dialogEnd button").click(function() {
 		onAgain(state);
-	});
-
-	setQuestions(state);
+	}); 
+	
 });
 
-function setQuestions(state){
-	var item = items[state.counter]
+function onStart(state){
+	setQuestions(state.counter);
+	$(".dialogStart").hide();
+	$(".dialogQuestions").show();
+}
+
+
+function setQuestions(counter){
+	var item = questions[counter];
 	
-	$(".question").html(item.question);
+	var dialogQuestions = $(".dialogQuestions");
+	dialogQuestions.find(".boxText").html(item.question);
 	
-	var choose = $(".choose");
+	var choose = dialogQuestions.find(".choose");
 	for (var i=0; i<4; i++){
-		$(choose[i]).val(items[state.counter].options[i]);
+		$(choose[i]).val(questions[counter].options[i]);
 	}
 }
 
 function onChoose(state, id){
 	var choose = $(".choose");
-	if (id == items[state.counter].correct){
+	if (id == questions[state.counter].correct){
 		$(choose[id]).addClass("borderTrue");
 		state.correctAnswers++;
 	} else {
 		$(choose[id]).addClass("borderFalse");
-		$(choose[items[state.counter].correct]).addClass("borderTrue");
+		$(choose[questions[state.counter].correct]).addClass("borderTrue");
 		state.wrongAnswers++;
 	}
 	choose.prop('disabled', true);
+	choose.addClass("disChoose");
+	$(".dialogQuestions button").removeClass("disableBoxButt");
+	$(".dialogQuestions button").addClass("enableBoxButt");
 	state.clicked = 1;
-}
-
-function onStart(state){
-	$(".dialogStart").addClass("hide");
 }
 
 
 function onNext(state){
 	
-	if (state.clicked == 0){
-		return;
+	if (state.clicked == 1){
+		state.counter++;
+		if (state.counter >= questions.length){
+			var dialogEnd = $(".dialogEnd");
+			dialogEnd.show();
+			dialogEnd.find(".boxText").append("Correct answers: ")
+							.append(state.correctAnswers)
+							.append("</br>")
+							.append("Wrong answers: ")
+							.append(state.wrongAnswers);
+			$(".dialogQuestions").hide();
+		} else {
+			setQuestions(state.counter);
+			enableOptions();
+			$(".choose").removeClass("disChoose");
+			$(".dialogQuestions button").addClass("disableBoxButt");
+			$(".dialogQuestions button").removeClass("enableBoxButt");	
+			state.clicked = 0;	
+		}
 	}
-	state.counter++;
-	
-	if (state.counter >= items.length){
-		$(".dialogEnd").addClass("show");
-		$(".dialogEndText").append("Correct answers: ")
-						.append(state.correctAnswers)
-						.append("</br>")
-						.append("Wrong answers: ")
-						.append(state.wrongAnswers);
-		return;
-	}
-	
-	setQuestions(state);
-	enableOptions();
-	state.clicked = 0;
 }
 
 function onAgain(state){
@@ -106,17 +93,18 @@ function onAgain(state){
 	state.correctAnswers = 0;
 	state.wrongAnswers = 0;
 	
-	setQuestions(state);
+	setQuestions(state.counter);
 	
-	$(".dialogEnd").removeClass("show");
-	$(".dialogEndText").empty();
+	var dialogEnd = $(".dialogEnd");
+	dialogEnd.hide();
+	dialogEnd.find(".boxText").empty();
+	$(".dialogQuestions").show();
 	enableOptions();
-
+	$(".choose").removeClass("disChoose");
 }
 
 function enableOptions(){
-	$(".choose").removeClass("borderFalse borderTrue");
-	$(".choose").prop('disabled', false);
+	$(".dialogQuestions .choose").removeClass("borderFalse borderTrue").prop('disabled', false);
 }
 
 
---
+++ questions.js
@@ -0,0 +1,123 @@
+var questions = 
+[
+	{ 
+		"question": "What is the Capital of Kazahstan?",
+		"options": ["Tashkent", "Sucre", "Astana", "Ashgabat"],
+		"correct": 2
+	 },
+	 {
+		"question": "Who is the fastest land animal?",
+		"options": ["Horse", "Cheetah", "Lion", "Kangaro"],
+		"correct": 1
+	 },
+	 {
+		"question": "What is the speed of sound?",
+		"options": ["340 m/s", "3.6 km/h", "1020 m/s", "32 m/s"],
+		"correct": 0
+	 },
+	  {
+		"question": "Which planet has the most moons?",
+		"options": ["Neptune", "Saturn", "Uranus", "Jupiter"],
+		"correct": 3
+	 },
+	 {
+		"question": "What is debut studio album by the American rock band Nirvana?",
+		"options": ["Come as you are", "Nevermind", "In Utero", "Bleach"],  
+		"correct": 3
+	 },
+	  {
+		"question": "What film didn't win 11 Oscar awards?",
+		"options": ["The Return of the King ", "Ben-Hur", "Gone With The Wind", "Titanic"],
+		"correct": 2
+	 },
+	  {
+		"question": "How many players are in a baseball team?",
+		"options": ["7", "11", "9", "10"],
+		"correct": 2
+	 },
+	  {
+		"question": "What is the heaviest living reptile?",
+		"options": ["Aldabra giant tortoise", "Orinoco crocodile", "Nile crocodile", "Saltwater crocodile"],
+		"correct": 3
+	 },
+	  {
+		"question": "During what Chinese dinasty was paper invented?",
+		"options": ["Han", "Quin", "Xin", "Ming"],
+		"correct": 0
+	 },
+	  {
+		"question": "In which century did Western Roman Empire end?",
+		"options": ["5 AD", "4 BC", "7 AD", "6 AD"],
+		"correct": 0
+	 },
+	  {
+		"question": "In which century did Francesco Petrarca live?",
+		"options": ["11", "12", "13", "14"],
+		"correct": 0
+	 },
+	  {
+		"question": "Who was FIFA World Cup winner 1994?",
+		"options": ["Germany", "France", "Brasil", "Argentina"],
+		"correct": 2
+	 },
+	   {
+		"question": "What is the capital of Bangladesh?",
+		"options": ["Hanoi", "Islamabad", "Dhaka", "Kathmandu"],
+		"correct": 2
+	 },
+	  {
+		"question": "Where is La Guajira Desert?",
+		"options": ["In Columbia", "In Peru", "In Argentina", "In Chile"],
+		"correct": 0
+	 },
+	  {
+		"question": "How much world's lakes are in Canada?",
+		"options": ["45%", "60%", "80%", "10%"],
+		"correct": 1
+	 },
+	   {
+		"question": "Which country has the youngest population in the world",
+		"options": ["Niger", "Mali", "Uganda", "Malawi"],
+		"correct": 0
+	 },
+	   {
+		"question": "Process that makes organisms better suited to their habitat is called",
+		"options": ["Co-evolution", "Speciation", "Co-operation", "Adaptation"],
+		"correct": 3
+	 },
+	  {
+		"question": "The amount of energy released when an electron is added to a neutral atom to form a negative ion is called",
+		"options": ["Electron affinity", "Electronegativity", "Ionization energy", "Electron configuration"],
+		"correct": 0
+	 },
+	  {
+		"question": "When did American Revolution ended?",
+		"options": ["1776", "1781", "1783", "1779"],
+		"correct": 1
+	 },
+	   {
+		"question": "Who was the drummer of the band The Doors?",
+		"options": ["Ray Manzarek", "John Densmore", "Robby Krieger", "Mike Robinson"],
+		"correct": 1
+	 },
+	 {
+		"question": "Who was the drummer of the band The Doors?",
+		"options": ["Ray Manzarek", "John Densmore", "Robby Krieger", "Mike Robinson"],
+		"correct": 1
+	 },
+	  {
+		"question": "Who said 'Happiness is the highest good' ?",
+		"options": ["Winston Churchill", "Richard Wayne", "Albert Einstein", " Aristotle"],
+		"correct": 3
+	 },
+	 {
+		"question": "Who painted 'Girl with a Pearl Earring'?",
+		"options": ["Sandro Botticelli", "Johannes Vermeer", "Gustav Klimt, "Henri Rousseau"],
+		"correct": 1
+	 },
+	  {
+		"question": "Which river runs through Paris?",
+		"options": ["Rhone", "Loire ", "Garonne", "Seine"],
+		"correct": 1
+	 }
+]
